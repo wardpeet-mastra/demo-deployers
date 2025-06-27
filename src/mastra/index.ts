06e4b801ef4df2c6e5103bca4f5840a631bb051e
wardpeet <ward@coding-tech.com>
2025-06-27 16:12:54 +0200

    enable deployers

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,6 +1,9 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { weatherAgent } from "./agents/weather-agent";
+import { NetlifyDeployer } from "@mastra/deployer-netlify";
+import { VercelDeployer } from "@mastra/deployer-vercel";
+// import { researchAgent } from "./agents/research-agent";
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
@@ -8,4 +11,5 @@ export const mastra = new Mastra({
     name: "Mastra",
     level: "info",
   }),
+  deployer: process.env.VERCEL ? new VercelDeployer() : new NetlifyDeployer(),
 });
